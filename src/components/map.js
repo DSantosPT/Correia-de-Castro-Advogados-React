0b72911b4416912b0b5486bea9d6267e48ffe0bc
DSantosPT <diogo_sasantos@hotmail.com>
2022-07-02 12:54:36 +0100

    Commit from TeleportHQ

--- src/components/map.js
+++ src/components/map.js
@@ -11,7 +11,7 @@ const Map = (props) => {
       data-lat="41.010035618871754"
       data-lng="-8.641739259541081"
       data-type="Map"
-      className="map-map"
+      className={`map-map ${props.rootClassName} `}
     >
       <iframe
         src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Avenida%20da%20R%C3%A9publica%20740%C2%BA,%202%C2%BA-s%2021,%204430-190%20Vila%20nova%20de%20Gaia%20+(Correia%20de%20Castro%20Advogados)&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
@@ -23,10 +23,12 @@ const Map = (props) => {
 
 Map.defaultProps = {
   initMap: () => {},
+  rootClassName: '',
 }
 
 Map.propTypes = {
   initMap: PropTypes.func,
+  rootClassName: PropTypes.string,
 }
 
 export default Map
